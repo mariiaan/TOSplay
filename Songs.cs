0b08aaf145e1a5f8168f62db424e3eaa15d056de
Marian W <strimearts@gmail.com>
2021-02-25 02:49:13 +0100

    Create Songs.cs

---
+++ Songs.cs
@@ -0,0 +1,9 @@
+namespace TOSPlay
+{
+    public static class Songs
+    {
+        /* Songs by Terry Davis */
+        public static readonly string templeOSTheme = "5eDEqFFetEEFqDeCDDEetCGF5eDEqFFetEEFqDeCDDEetCGF5eDCqDE4eAA5etEEFEDG4B5DCqF5eDCqDE4eAA5etEEFEDG4B5DCqF";
+        public static readonly string talonsTheme = "5eCGFsD4A5e.C4sG5eGDqCDeGsGG4qG5eCGFsD4A5e.C4sG5eGDqCDeGsGG4qG5eGECGC4A5FCsC4B5C4B5e.GsG4qGB5eGECGC4A5FCsC4B5C4B5e.GsG4qGB";
+    }
+}
